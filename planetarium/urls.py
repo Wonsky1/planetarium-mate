271400d102b36d02810e77ede6a497277a5dad8c
wonsky <pidborskii17@gmail.com>
2023-12-11 23:00:57 +0200

    Implemented endpoints for reservations

--- planetarium/urls.py
+++ planetarium/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from planetarium.views import AstronomyShowViewSet, ShowThemeViewSet, PlanetariumDomeViewSet, ShowSessionViewSet
+from planetarium.views import AstronomyShowViewSet, ShowThemeViewSet, PlanetariumDomeViewSet, ShowSessionViewSet, \
+    ReservationViewSet
 
 router = routers.DefaultRouter()
 router.register("astronomy_shows", AstronomyShowViewSet)
 router.register("show_themes", ShowThemeViewSet)
 router.register("planetarium_domes", PlanetariumDomeViewSet)
 router.register("show_sessions", ShowSessionViewSet)
+router.register("reservations", ReservationViewSet)
 
 urlpatterns = [
     path("", include(router.urls))
