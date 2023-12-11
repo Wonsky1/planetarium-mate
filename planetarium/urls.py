621eff790f45251e90de51850f6a1a7db2737c0f
wonsky <pidborskii17@gmail.com>
2023-12-11 13:50:16 +0200

    Implemented endpoints for Themes and Show

--- planetarium/urls.py
+++ planetarium/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
+from planetarium.views import AstronomyShowViewSet, ShowThemeViewSet
+
+router = routers.DefaultRouter()
+router.register("astronomy_shows", AstronomyShowViewSet)
+router.register("show_themes", ShowThemeViewSet)
 
 urlpatterns = [
-
+    path("", include(router.urls))
 ]
 
 app_name = "planetarium"
