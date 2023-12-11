82aefa73eacb84eb45477f07804cd9367245ec80
wonsky <pidborskii17@gmail.com>
2023-12-11 14:34:51 +0200

    Implemented endpoints for ShowSession

--- planetarium/urls.py
+++ planetarium/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from planetarium.views import AstronomyShowViewSet, ShowThemeViewSet, PlanetariumDomeViewSet
+from planetarium.views import AstronomyShowViewSet, ShowThemeViewSet, PlanetariumDomeViewSet, ShowSessionViewSet
 
 router = routers.DefaultRouter()
 router.register("astronomy_shows", AstronomyShowViewSet)
 router.register("show_themes", ShowThemeViewSet)
 router.register("planetarium_domes", PlanetariumDomeViewSet)
+router.register("show_sessions", ShowSessionViewSet)
 
 urlpatterns = [
     path("", include(router.urls))
