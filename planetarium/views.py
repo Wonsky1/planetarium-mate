c46a34200ae6e9abd16bc4cf36f6b9aebd7c5ad4
wonsky <pidborskii17@gmail.com>
2023-12-11 13:50:02 +0200

    Implemented views for Themes and Show

--- planetarium/views.py
+++ planetarium/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from planetarium.models import AstronomyShow, ShowTheme
+from planetarium.serializers import AstronomyShowSerializer, AstronomyShowListSerializer, ShowThemeSerializer
+
+
+class ShowThemeViewSet(viewsets.ModelViewSet):
+    queryset = ShowTheme.objects.all()
+    serializer_class = ShowThemeSerializer
+
+
+class AstronomyShowViewSet(viewsets.ModelViewSet):
+    queryset = AstronomyShow.objects.prefetch_related("themes")
+    serializer_class = AstronomyShowSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return AstronomyShowListSerializer
+        return AstronomyShowSerializer
