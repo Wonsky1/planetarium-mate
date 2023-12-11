50539bb539d963e39555b272a72b35dc7ac9188c
wonsky <pidborskii17@gmail.com>
2023-12-11 12:41:25 +0200

    settings.py update, provided user model, rest framework into apps

--- planetarium_service/settings.py
+++ planetarium_service/settings.py
@@ -37,9 +37,14 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "user",
     "planetarium",
 ]
+
+
+AUTH_USER_MODEL = "user.User"
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -113,13 +118,5 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-
-STATIC_URL = "static/"
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
