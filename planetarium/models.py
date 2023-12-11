f83869c06c6b8a4d95c40513b33e135678dd2899
wonsky <pidborskii17@gmail.com>
2023-12-11 12:36:37 +0200

    Created models and made migrations

--- planetarium/models.py
+++ planetarium/models.py
@@ -1,3 +1,77 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class ShowTheme(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class AstronomyShow(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    themes = models.ManyToManyField(ShowTheme, related_name="astronomy_shows")
+
+    def __str__(self):
+        return self.title
+
+
+class PlanetariumDome(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField  # TODO: IMPLEMENT CONSTRAINS
+
+    def __str__(self):
+        return self.name
+
+
+class ShowSession(models.Model):
+    astronomy_show = models.ForeignKey(
+        AstronomyShow,
+        on_delete=models.CASCADE,
+        related_name="show_sessions"
+    )
+    planetarium_dome = models.ForeignKey(
+        PlanetariumDome,
+        on_delete=models.CASCADE,
+        related_name="show_sessions"
+    )
+    show_time = models.DateTimeField()
+
+    def __str__(self):
+        return (f"Session {self.show_time},"
+                f" {self.astronomy_show.title},"
+                f" {self.planetarium_dome.name}")
+
+
+class Reservation(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name="reservations"
+    )
+
+    def __str__(self):
+        return f"Reservation {self.created_at}"
+
+
+class Ticket(models.Model):
+    row = models.IntegerField()
+    seat = models.IntegerField()
+    show_session = models.ForeignKey(
+        ShowSession,
+        on_delete=models.CASCADE,
+        related_name="tickets"
+    )
+    reservation = models.ForeignKey(
+        Reservation,
+        on_delete=models.CASCADE,
+        related_name="tickets"
+    )
+
+    def __str__(self):
+        return (f"Ticket on {self.show_session.planetarium_dome.name} "
+                f"row: {self.row}, seat: {self.seat}")
