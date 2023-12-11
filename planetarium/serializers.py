5bfcb274f225dd997e35df761410791d8228a046
wonsky <pidborskii17@gmail.com>
2023-12-11 14:34:29 +0200

    Implemented serializers for list and other views for ShowSession

--- planetarium/serializers.py
+++ planetarium/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 
-from planetarium.models import ShowTheme, AstronomyShow, PlanetariumDome
+from planetarium.models import ShowTheme, AstronomyShow, PlanetariumDome, ShowSession
 
+
+# TODO: permission_class
 
 class ShowThemeSerializer(serializers.ModelSerializer):
 
@@ -25,4 +27,33 @@ class PlanetariumDomeSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = PlanetariumDome
-        fields = ("id", "name", "rows", "seats_in_row")
+        fields = ("id", "name", "rows", "seats_in_row", "capacity")
+
+
+class ShowSessionSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ShowSession
+        fields = ("id", "astronomy_show", "planetarium_dome", "show_time")
+
+
+class ShowSessionListSerializer(ShowSessionSerializer):
+    show_title = serializers.CharField(source="astronomy.title", read_only=True)
+    planetarium_dome_name = serializers.CharField(
+        source="planetarium_dome.name", read_only=True
+    )
+    planetarium_dome_capacity = serializers.IntegerField(
+        source="planetarium_dome.capacity", read_only=True
+    )
+    tickets_available = serializers.IntegerField(read_only=True)
+
+    class Meta:
+        model = ShowSession
+        fields = (
+            "id",
+            "show_time",
+            "show_title",
+            "planetarium_dome_name",
+            "planetarium_dome_capacity",
+            "tickets_available",
+        )
