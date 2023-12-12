6b57e0b680ff98cc038ca9f0d9b4cddbdd8cde28
wonsky <pidborskii17@gmail.com>
2023-12-12 11:07:53 +0200

    Created migrations, implemented validation into serializers

--- planetarium/serializers.py
+++ planetarium/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from django.db import transaction
+from django.core.exceptions import ValidationError
 
 from planetarium.models import (
     ShowTheme,
@@ -70,6 +71,16 @@ class ShowSessionListSerializer(ShowSessionSerializer):
 
 
 class TicketSerializer(serializers.ModelSerializer):
+    def validate(self, attrs):
+        data = super(TicketSerializer, self).validate(attrs=attrs)
+        Ticket.validate_ticket(
+            attrs["row"],
+            attrs["seat"],
+            attrs["show_session"].planetarium_dome,
+            ValidationError
+        )
+        return data
+
     class Meta:
         model = Ticket
         fields = ("id", "row", "seat", "show_session")
