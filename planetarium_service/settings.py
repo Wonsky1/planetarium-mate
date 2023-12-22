dfe1862b898b99ef21fd81f6d38bc2de7dd8092d
wonsky <pidborskii17@gmail.com>
2023-12-22 19:42:19 +0200

    replaced secret key

--- planetarium_service/settings.py
+++ planetarium_service/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-9)c3f6k@^_c-)-(d1w%va=ev9hr23@*b-i1e8xz#9k^xj^8^+9")
+SECRET_KEY = os.environ.get("SECRET_KEY", "some-secret-key-9)c3f6k@^_c-)-(d1w%va=ev9hr23@*b-i1e8xz#9k^xj^8^+9")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
